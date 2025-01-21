3638eae23587ee191301abda53549716de746741
Leonardo Dominici <leonardodominici01@gmail.com>
2025-01-21 23:52:55 +0100

    Increased processes for Assignment 1

--- Assignment 2/Task 1/job.sh
+++ Assignment 2/Task 1/job.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks=1 
 #SBATCH --cpus-per-task=32
 #SBATCH --time=00:10:00 
-#SBATCH --partition=cbuild 
+#SBATCH --partition=rome 
 #SBATCH --output=pi_%j.out 
 #SBATCH --error=pi_%j.err 
 module purge 
@@ -12,7 +12,7 @@ module load 2022
 module load GCCcore/11.3.0 
 echo "OpenMP parallelism" 
 echo 
-for ncores in `seq 8 4 48` 
+for ncores in `seq 0 4 512` 
 do 
 export OMP_NUM_THREADS=$ncores 
 echo "CPUS: " $OMP_NUM_THREADS 
